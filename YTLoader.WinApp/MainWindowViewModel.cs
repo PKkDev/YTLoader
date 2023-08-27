cb2d4e982e2a10d065c6a741b57444b8d30e9db1
Kirill <prodevkir@mail.ru>
2023-08-27 20:17:13 +0400

    update

--- YTLoader.WinApp/MainWindowViewModel.cs
+++ YTLoader.WinApp/MainWindowViewModel.cs
@@ -4,6 +4,11 @@ using YTLoader.Core;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Threading.Tasks;
+using System.Text;
+using Windows.System;
+using Windows.Storage;
+using System;
+using System.IO;
 
 namespace YTLoader.WinApp
 {
@@ -42,11 +47,34 @@ namespace YTLoader.WinApp
             set => SetProperty(ref _keywords, value);
         }
 
+        private StorageFolder _currentFolder;
+        public StorageFolder CurrentFolder
+        {
+            get => _currentFolder;
+            set => SetProperty(ref _currentFolder, value);
+        }
+
         public RelayCommand OnAnalyzVideo { get; set; }
+        public RelayCommand OnDownloadVideo { get; set; }
+        public RelayCommand OnOpenCurrentFolderCmd { get; set; }
 
         private readonly YouTubeClient youTubeClient;
 
         private VideoInfo videoInfo;
+
+        private string _formatInfoTxt;
+        public string FormatInfoTxt
+        {
+            get => _formatInfoTxt;
+            set => SetProperty(ref _formatInfoTxt, value);
+        }
+
+        private string _downloadMsg;
+        public string DownloadMsg
+        {
+            get => _downloadMsg;
+            set => SetProperty(ref _downloadMsg, value);
+        }
 
         public ObservableCollection<FormatInfo> AllFormats { get; set; }
         private FormatInfo _selectedFormat;
@@ -63,16 +91,42 @@ namespace YTLoader.WinApp
         public MainWindowViewModel()
         {
             youTubeClient = new();
+            youTubeClient.ProgressChanged += (long? totalFileSize, long totalBytesDownloaded, double? progressPercentage) =>
+            {
+                DownloadMsg = $"{progressPercentage}% ({totalBytesDownloaded}/{totalFileSize})";
+            };
+
             AllFormats = new();
-            InputVideoUrl = "https://www.youtube.com/watch?v=LZvTEecjxos";
+            CurrentFolder = ApplicationData.Current.LocalCacheFolder;
+            //InputVideoUrl = "https://www.youtube.com/watch?v=LZvTEecjxos";
+            InputVideoUrl = "https://www.youtube.com/watch?v=FbIRBPhdbYg";
 
             OnAnalyzVideo = new RelayCommand(async () => await AnalyzVideo());
+            OnDownloadVideo = new RelayCommand(async () => await DownloadVideo());
+            OnOpenCurrentFolderCmd = new RelayCommand(async () =>
+            {
+                if (CurrentFolder != null)
+                    await Launcher.LaunchFolderAsync(CurrentFolder);
+            });
         }
 
         private void OnViewDetailForFormat(FormatInfo format)
         {
-            if (SelectedFormat != null && SelectedFormat.FormatCode != format.FormatCode)
+            if (SelectedFormat == null || SelectedFormat.FormatCode != format.FormatCode)
             {
+                StringBuilder sb = new();
+                sb.AppendLine($"FormatCode: {format.FormatCode}");
+                sb.AppendLine($"Fps: {format.Fps}");
+                sb.AppendLine($"AudioBitrate: {format.AudioBitrate}");
+                sb.AppendLine($"Resolution: {format.Resolution}");
+                sb.AppendLine($"QualityLabel: {format.QualityLabel}");
+                sb.AppendLine($"MimeType: {format.MimeType}");
+                sb.AppendLine($"Codecs: {string.Join(',', format.Codecs)}");
+                sb.AppendLine($"Format: {format.Format}");
+                sb.AppendLine($"AudioFormat: {format.AudioFormat}");
+                sb.AppendLine($"AdaptiveKind: {format.AdaptiveKind}");
+                sb.AppendLine($"AudioQuality: {format.AudioQuality}");
+                FormatInfoTxt = sb.ToString();
             }
         }
 
@@ -91,6 +145,20 @@ namespace YTLoader.WinApp
             {
                 AllFormats.Add(format);
             };
+        }
+
+        private async Task DownloadVideo()
+        {
+            if (SelectedFormat == null || videoInfo == null)
+            {
+                return;
+            }
+
+            var bytes = await youTubeClient.GetBytes(SelectedFormat);
+
+            StorageFile file = await CurrentFolder.CreateFileAsync($"{videoInfo.VideoName}{SelectedFormat.FileExtension}", CreationCollisionOption.GenerateUniqueName);
+            using Stream stream = await file.OpenStreamForWriteAsync();
+            await stream.WriteAsync(bytes, 0, bytes.Length);
         }
     }
 }
